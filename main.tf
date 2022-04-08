191828bb1d362fb75497576bb5532fae125bb419
gustavo <gustavo.kuno@linx.com.br>
2022-04-08 11:49:54 -0300

    .

--- main.tf
+++ main.tf
@@ -107,9 +107,15 @@ resource "azurerm_windows_virtual_machine" "lab" {
     azurerm_network_interface.lab.id,
   ]
 
+  winrm_listener {
+    protocol = "Http"
+  }
+
   os_disk {
+    name                 = "lab-machine2_OsDisk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    disk_size_gb         = 128
   }
 
   source_image_reference {
@@ -127,44 +133,6 @@ resource "azurerm_windows_virtual_machine" "lab" {
     azurerm_network_interface.lab
   ]
 }
-
-#resource "azurerm_virtual_machine_extension" "vm-winrm" {
-#  name                 = "WinRM-Ansible"
-#  virtual_machine_id   = azurerm_windows_virtual_machine.lab.id
-#  publisher            = "Microsoft.Compute"
-#  type                 = "CustomScriptExtension"
-#  type_handler_version = "1.9"
-#
-#  protected_settings = <<EOF
-#    {
-#        "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(data.template_file.tf.rendered)}')) | Out-File -filepath script.ps1\" && powershell -ExecutionPolicy Unrestricted -File script.ps1"
-#    }
-#    EOF
-#  depends_on = [
-#    azurerm_windows_virtual_machine.lab
-#  ]
-#}
-
-
-/*
-resource "azurerm_virtual_machine_extension" "vm-winrm" {
-  name                 = "WinRM-Ansible"
-  virtual_machine_id   = azurerm_windows_virtual_machine.lab.id
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.9"
-
-  protected_settings = <<SETTINGS
-  {
-     "commandToExecute": "powershell -encodedCommand ${textencodebase64(file("script.ps1"), "UTF-16LE")}"
-  }
-  SETTINGS
-
-  depends_on = [
-    azurerm_windows_virtual_machine.lab
-  ]
-}
-*/
 
 resource "azurerm_virtual_machine_extension" "vm-winrm" {
   name                       = "WinRM-Ansible"
@@ -185,4 +153,50 @@ resource "azurerm_virtual_machine_extension" "vm-winrm" {
   ]
 }
 
-# "commandToExecute": "powershell -encodedCommand ${textencodebase64(file("script.ps1"), "UTF-16LE")}"
+
+resource "azurerm_storage_account" "storage_account" {
+  name                = "${var.stg-name}${lower(random_id.kv-name.hex)}"
+  resource_group_name = azurerm_resource_group.rg-lab.name
+  location            = azurerm_resource_group.rg-lab.location
+
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Cool"
+
+  enable_https_traffic_only = true
+}
+
+resource "azurerm_storage_share" "files" {
+  name                 = "files"
+  storage_account_name = azurerm_storage_account.storage_account.name
+  quota                = 10
+}
+
+/*
+locals {
+  connect_file_share_script = templatefile("connect-azure-file-share.tpl.ps1", {
+    storage_account_file_host = azurerm_storage_account.storage_account.primary_file_host
+    storage_account_name      = azurerm_storage_account.storage_account.name
+    storage_account_key       = azurerm_storage_account.storage_account.primary_access_key
+    file_share_name           = azurerm_storage_share.files.name
+    drive_letter              = "Z"
+  })
+}
+
+resource "azurerm_virtual_machine_extension" "attach_file_share" {
+  name                       = "attach_file_share"
+  virtual_machine_id         = azurerm_windows_virtual_machine.lab.id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.10"
+  auto_upgrade_minor_version = true
+
+  settings = jsonencode({
+    commandToExecute = "powershell -EncodedCommand ${textencodebase64(local.connect_file_share_script, "UTF-16LE")}"
+  })
+
+  depends_on = [azurerm_storage_share.files]
+}
+
+*/
